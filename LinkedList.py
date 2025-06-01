e08c023682c908eab1adfdf0074e7446d4c6eddd
Aditya Ranjan <adiranjan2019@gmail.com>
2025-06-01 11:33:17 +0100

    Added Linkedlist class

--- LinkedList.py
+++ LinkedList.py
@@ -1,8 +1,26 @@
 
 class ListNode:
-    def __init__(self):
-        self.val = 0
+    def __init__(self,data = 0):
+        self.val = data
         self.next = None
 
     
     
+
+class LinkedList:
+    def __init__(self):
+        #Head points to start of linked list
+        self.head = ListNode()
+        
+
+    def add(self,data):
+        new_node = ListNode(data)
+        cur = self.head
+        while cur.next != None:
+            cur = cur.next
+
+        cur.next = new_node
+
+
+    def length():
+        
