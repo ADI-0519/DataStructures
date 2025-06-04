d3e212eeb3cd3c8033e013b48cd55fc87429754c
Aditya Ranjan <adiranjan2019@gmail.com>
2025-06-04 13:47:36 +0100

    Update Sorting Algos.py

--- Sorting Algos.py
+++ Sorting Algos.py
@@ -9,8 +9,8 @@ def bubble_sort(arr):
                 sorted = False
                 
 def insertion_sort(arr):
-    for i in range(len(arr)):
-        for j in range(i,len(arr)-1):
+    for i in range(1,len(arr)):
+        for j in range(0,i):
             if arr[i]>arr[j]:
                 arr[i],arr[j] = arr[j],arr[i]
 
