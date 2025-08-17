90b8fca4741d44af7fd0dfe5caff785aa026865e
Aditya Ranjan <adiranjan2019@gmail.com>
2025-08-18 00:46:25 +0100

    Add quick reference rules for bit manipulation
    Added a set of concise comments summarizing common bit manipulation tricks and their effects. This serves as a handy reference for understanding and applying bitwise operations.

--- BitManipulation.py
+++ BitManipulation.py
@@ -19,3 +19,11 @@ print(5 << 2) # --> Performs a arithmetic left shift
 print(5 >> 1) # --> Performs a signed right shift
 
 print(hex(5)) # --> '0x5'
+
+# quick rules:
+# n & n-1 removes the last set bit
+# n & -n gives the last set bit
+# n | n-1 sets all bits to the right of the last set bit
+# n ^ -1 flips all bits
+# n << 1 multiplies by 2
+# n >> 1 divides by 2
